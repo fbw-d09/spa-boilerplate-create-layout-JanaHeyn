da622a9cf985dd1ff9a907e3cbbd78411caf0923
JanaHeyn <jana.heyn@web.de>
2023-06-12 14:44:56 +0200

    Solve task

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const List  = () => {
-  const navlinkItems = []; // only update the value of this array
+  const navlinkItems = [
+    <li classNameName="nav-item">
+      <a classNameName="nav-link" href="#">Link to google.com</a>
+    </li>,
+    <li className="nav-item">
+      <a className="nav-link" href="#">Link to facebook.com</a>
+    </li>,
+    <li className="nav-item">
+      <a className="nav-link" href="#">Link to amazon.com</a>
+    </li>,
+  ]; // only update the value of this array
 
   return (
     <ul className="nav">{navlinkItems}</ul>
